2393e69e837a754f7acd768f495648e7d0ddbd39
cz848 <cz848@qq.com>
2019-10-21 08:26:08 +0800

    重构代码

--- index.js
+++ index.js
@@ -2,64 +2,80 @@
  * 统一处理日期或格式化输出
  */
 
-const characterRegExp = /[ymdwhisau]/gi;
-const addFormatsRegExp = /^([+-]?)((?:\d\.)?\d+)([ymdwhis]?)$/i;
+const characterRegExp = /ms|[ymdwhisau]/gi;
+const addFormatsRegExp = /^([+-]?(?:\d\.)?\d+)(ms|[ymdhis])?$/i;
 const I18N = {
   weekdays: ['日', '一', '二', '三', '四', '五', '六'],
   apm: ['上午', '下午'],
 };
 
-// 位数不够前补0
+// 位数不够前补0，为了更好的兼容，用slice替代padStart
 const zeroFill = (number, targetLength = 2) => `00${number}`.slice(-targetLength);
 
-// 是否为datetime的实例
+// 首字母大写
+const capitalize = (str = '', prefix = '') => `${prefix}${str.replace(/(^[a-z])/, $1 => $1.toUpperCase())}`;
+
+// 是否为dateio的实例
 // eslint-disable-next-line no-use-before-define
-const isInstance = input => input instanceof DateTime;
+const isInstance = input => input instanceof DateIO;
 
 // 转换为可识别的日期格式
 const toDate = input => {
   if (!input) return new Date();
-  if (isInstance(input)) return input;
-  // if (typeof input === 'function') toDate(input());
+  if (isInstance(input)) return input.$date;
+  if (input instanceof Date) return input;
   if (typeof input === 'string') return new Date(input.replace(/[.-]/g, '/'));
-  // TODO:与原生行为不符，需优化
-  if (Array.isArray(input)) return new Date(...input);
+  // TODO:与原生行为有出入
+  if (Array.isArray(input) && input.length !== 1) return new Date(...input);
   return new Date(input);
 };
 
-class DateTime {
-  constructor(input) {
-    this.I18N = I18N;
-    this.origin = input;
-    this.init();
+class DateIO {
+  constructor(input = new Date()) {
+    this.i18n();
+    this.init(input);
   }
 
-  init() {
-    this.$date = toDate(this.origin);
-    if (Number.isNaN(this.valueOf())) throw new Error(`Invalid Date: ${this.origin}, type: ${typeof this.origin}.`);
+  init(input) {
+    if (input) {
+      this.$date = toDate(input);
+      if (Number.isNaN(this.valueOf())) throw new Error(`Invalid Date: "${input}", type: "${typeof input}".`);
+    }
+    this.origin = this.valueOf();
     return this;
   }
 
-  toDate() {
-    return toDate(this.$date);
+  i18n(config) {
+    this.I18N = { ...I18N, ...config };
+    return this;
+  }
+
+  $set(type, ...input) {
+    this.$date[`set${capitalize(type)}`](...input);
+    return this.init();
   }
 
   // 年 (4位)
-  // 1970...2018
-  Y() {
-    return this.$date.getFullYear();
-  }
-
-  // 年 (2位)
-  // 70...18
+  // 1970...2019
   y(...input) {
-    return input.length ? this.$date.setFullYear(...input) : this.Y().toString().slice(-2);
+    if (input.length > 1) input[1] -= 1;
+    return input.length ? this.$set('fullYear', ...input) : this.$date.getFullYear();
   }
 
-  // 修正偏移后的月
+  // 年 (4位)
+  // 1970...2019
+  Y() {
+    return this.y();
+  }
+
+  // 加偏移后的月
   // 1...12
   m(...input) {
-    return input.length ? this.$date.setMonth(...input) : this.$date.getMonth() + 1;
+    if (input.length) {
+      input[0] -= 1;
+      return this.$set('month', ...input);
+    }
+    return this.$date.getMonth() + 1;
   }
 
   // 月 (前导0)
@@ -71,7 +87,7 @@ class DateTime {
   // 日
   // 1...31
   d(...input) {
-    return input.length ? this.$date.setDate(...input) : this.$date.getDate();
+    return input.length ? this.$set('date', ...input) : this.$date.getDate();
   }
 
   // 日 (前导0)
@@ -95,7 +111,7 @@ class DateTime {
   // 24小时制
   // 0...23
   h(...input) {
-    return input.length ? this.$date.setHours(...input) : this.$date.getHours();
+    return input.length ? this.$set('hours', ...input) : this.$date.getHours();
   }
 
   // 24小时制 (前导0)
@@ -107,7 +123,7 @@ class DateTime {
   // 分
   // 0...59
   i(...input) {
-    return input.length ? this.$date.setMinutes(...input) : this.$date.getMinutes();
+    return input.length ? this.$set('minutes', ...input) : this.$date.getMinutes();
   }
 
   // 分 (前导0)
@@ -119,13 +135,19 @@ class DateTime {
   // 秒
   // 0...59
   s(...input) {
-    return input.length ? this.$date.setSeconds(...input) : this.$date.getSeconds();
+    return input.length ? this.$set('seconds', ...input) : this.$date.getSeconds();
   }
 
   // 秒 (前导0)
   // 00...59
   S() {
     return zeroFill(this.s());
+  }
+
+  // 毫秒数
+  // 0...999
+  ms(...input) {
+    return input.length ? this.$set('milliseconds', ...input) : this.$date.getMilliseconds();
   }
 
   // 上下午
@@ -140,29 +162,33 @@ class DateTime {
     return this.a().toUpperCase();
   }
 
-  // 毫秒 (unix格式)
-  // 0...1542759768000
+  // 毫秒时间戳 (unix格式)
+  // 0...1571136267050
   u(input) {
-    return input ? this.$date.setMilliseconds(input) : this.valueOf();
+    return input ? this.init(input) : this.valueOf();
   }
 
-  // 秒 (unix格式)
+  // 秒时间戳 (unix格式)
   // 0...1542759768
-  U() {
-    return Math.floor(this.u() / 1000);
+  U(input) {
+    return input ? this.u(input * 1000) : Math.floor(this.u() / 1000);
   }
 
-  // 获取以上格式的日期
-  get(formats) {
-    const fs = this[formats];
+  // 获取以上格式的日期，每个unit对应其中一种格式
+  get(unit = '') {
+    const fs = this[unit];
     return typeof fs === 'function' ? fs.call(this) : undefined;
   }
 
   // 设置以上格式的日期
-  set(formats, ...input) {
-    if (typeof this[formats.toLowerCase()] !== 'function') throw new Error(`Invalid set format: ${formats}.`);
-    this.origin = this[formats.toLowerCase()](...input);
-    return this.init();
+  set(unit = '', ...input) {
+    const key = unit.toLowerCase();
+    if (typeof this[key] !== 'function') throw new Error(`Invalid set format: "${unit}".`);
+    return this[key](...input);
+  }
+
+  toDate() {
+    return toDate(this.$date);
   }
 
   toString() {
@@ -178,84 +204,85 @@ class DateTime {
   }
 
   clone() {
-    return new DateTime(this.origin);
+    return new DateIO(this.origin);
   }
 
   // 利用格式化串格式化日期
-  format(formats) {
+  format(formats = 'Y-M-D H:I:S') {
     // 执行相应格式化
-    return (formats || '').replace(characterRegExp, key => {
+    return (formats).replace(characterRegExp, key => {
       const fs = this[key];
-      return typeof fs === 'function' ? fs.call(this) : fs || this.$date;
+      return typeof fs === 'function' ? fs.call(this) : fs || key;
     });
   }
 
-  // 返回两个日期的差值，精确到秒
-  // formats: s: seconds(default)|i: minutes|h: hours|d: days|m:month(~)|y:year(~)
-  diff(input, formats) {
+  // 返回两个日期的差值，精确到毫秒
+  // unit: ms: milliseconds(default)|s: seconds|i: minutes|h: hours|d: days|m:months(in 30 days)|y:years(in 360 days)
+  // isFloat: 是否返回小数
+  diff(input, unit = 'ms', isFloat = false) {
     const time = toDate(input);
-    let diff = Math.floor((time - this) / 1000);
+    let diff = this - time;
     /* eslint-disable no-fallthrough */
-    switch (formats) {
-      case 'y':
+    switch (unit) {
+      case 'y': // ~
         diff /= 12;
-      case 'm':
+      case 'm': // ~
         diff /= 30;
-      // case 'w':
-      //   diff /= 7;
       case 'd':
         diff /= 24;
       case 'h':
         diff /= 60;
       case 'i':
         diff /= 60;
+      case 's':
+        diff /= 1000;
       default:
     }
     /* eslint-enable no-fallthrough */
 
-    return diff;
+    return isFloat ? diff : Math.floor(diff);
   }
 
-  to(input, formats = 's') {
+  // 对日期进行+-运算，默认精确到毫秒，可传小数
+  // input: '7d', '-1m', '10y', '5.5h'等或数字。
+  // unit: 'y', 'm', 'd', 'h', 'i', 's', 'ms'。
+  add(input, unit = 'ms') {
     const pattern = String(input).match(addFormatsRegExp);
-    if (!pattern) throw new Error(`Invalid to format: ${formats}.`);
-    const sign = pattern[1] === '-' ? -1 : 1;
-    const getFormat = (pattern[3] || formats);
-    const setFormat = getFormat.toLowerCase();
-    let number = Number(pattern[2]);
-    number = setFormat === 'm' ? (number - 1) : number;
-    const decimal = number.toString().split('.')[1];
+    if (!pattern) throw new Error(`Invalid to input: "${input}".`);
+    const mapUnit = (pattern[2] || unit).toLowerCase();
+    const number = Number(pattern[1]);
     const maps = {
-      i: 60,
-      h: 3600,
-      d: 86400,
-      w: 86400 * 7,
-      m: 86400 * 30, // ~
-      y: 86400 * 360, // ~
+      ms: 1,
+      s: 1e3,
+      i: 6e4,
+      h: 36e5,
+      d: 864e5,
+      m: 864e5 * 30, // ~
+      y: 864e5 * 360, // ~
     };
-    // 兼容小数(主要w以下~)
-    if (decimal) return this.set('u', sign * number * maps[setFormat] * 1000);
-    return this.set(setFormat, sign * number + Number(this.get(getFormat)));
+
+    return this.set('u', number * maps[mapUnit] + this.origin);
   }
 
+  subtract(input, unit = 'ms') {
+    return this.add(`-${input}`, unit);
+  }
+
+  // 计算某个月有几天
   daysInMonth() {
-    return this.to('1m').set('d', 0).d();
+    return this.add('1m').set('d', 0).d();
   }
 
-  isSame(input, formats = 'U') {
-    const thisTime = this.get(formats);
-    const thatTime = new DateTime(input).get(formats);
+  // 比较两个同格式的日期是否相同，默认精确到毫秒
+  isSame(input, unit = 'u') {
+    const thisTime = this.get(unit);
+    const thatTime = new DateIO(input).get(unit);
     return thisTime !== undefined && thatTime !== undefined && +thisTime === +thatTime;
-  }
-
-  i18n(conf = {}) {
-    this.I18N = { ...I18N, ...conf };
-    return this;
   }
 }
 
-const datetime = input => (isInstance(input) ? input.clone() : new DateTime(input));
+const dateio = input => (isInstance(input) ? input.clone() : new DateIO(input));
 
-datetime.prototype = DateTime.prototype;
+dateio.prototype = DateIO.prototype;
 
-export default datetime;
+export default dateio;
