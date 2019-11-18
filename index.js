4b9c8314e384c5eb0ad71d1afeccc84aae485cab
Tyler Chao <tyler.chao@verystar.cn>
2019-11-18 14:37:00 +0800

    统一写法

--- index.js
+++ index.js
@@ -4,29 +4,29 @@
  * github: https://github.com/cz848/dateio
  */
 
-const characterRegExp = /ms|[ymdwhisu]/gi;
-
 // 位数不够前补0，为了更好的兼容，用slice替代padStart
-const zeroFill = (number, targetLength = 2) => `00${number}`.slice(-targetLength);
+const zeroFill = (number, targetLength) => `00${number}`.slice(-targetLength || -2);
+
+// 匹配不同方法的正则
+const characterRegExp = /MS|ms|[YMDWHISAUymdwhisau]/g;
 
 // 转换为可识别的日期格式
 const toDate = input => {
-  if (!input) return new Date();
-  if (input instanceof Date) return input;
-  if (typeof input === 'string') return new Date(input.replace(/[.-]/g, '/'));
-  // TODO:与原生行为有出入
+  if (!(input || input === 0)) return new Date();
+  // fix: ISOString
+  if (typeof input === 'string' && !/Z$/i.test(input)) return new Date(input.replace(/-/g, '/'));
+  // TODO: 与原生行为有出入
   if (Array.isArray(input) && input.length !== 1) return new Date(...input);
   return new Date(input);
 };
 
 class DateIO {
-  constructor(input = new Date()) {
+  constructor(input) {
     this.init(input);
   }
 
   init(input) {
     this.$date = toDate(input);
-    if (Number.isNaN(this.valueOf())) throw new Error('Invalid Date');
     return this;
   }
 
@@ -56,7 +56,7 @@ class DateIO {
     } else if (unit === 'y') {
       res = res.toString().slice(-2);
     } else if (unit === 'U') {
-      res = Math.floor(res / 1000);
+      res = Math.round(res / 1000);
     }
     return res;
   }
@@ -70,8 +70,8 @@ class DateIO {
   }
 
   // 利用格式化串格式化日期
-  format(formats = 'Y-M-D H:I:S') {
-    return (formats).replace(characterRegExp, unit => (this.get(unit) || unit));
+  format(formats) {
+    return String(formats || 'Y-M-D H:I:S').replace(characterRegExp, unit => this.get(unit));
   }
 }
 
