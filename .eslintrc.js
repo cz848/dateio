0974033c9e34b7e9d84e0e1e54ce134073255954
Tyler Chao <tyler.chao@verystar.cn>
2020-07-07 19:35:56 +0800

    perf: 精简代码

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  // 指定解析器的选项
+  parserOptions: {
+    // eslint的解析器，需要安装对应的包
+    parser: 'babel-eslint',
+    // 指定es版本
+    ecmaVersion: 2019,
+    // 使用es模块
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    // promise 推荐规则
+    'plugin:promise/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['jest'],
+  env: {
+    'jest/globals': true,
+  },
+  settings: {},
+  rules: {
+    // 要求箭头函数的参数在需要时使用圆括号
+    'arrow-parens': ['error', 'as-needed'],
+    // 允许函数根据代码分支有不同的return行为
+    'consistent-return': 'off',
+    // 要求或禁止命名的 function 表达式
+    'func-names': ['error', 'as-needed'],
+    // 一行最大长度
+    'max-len': ['error', 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+    // 禁用console
+    'no-console': 'off',
+    // 禁用debugger
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 禁止对 function 的参数进行重新赋值
+    'no-param-reassign': ['error', {
+      props: false,
+    }],
+    // 除for循环外不允许++写法
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    // 强制大括号内换行符的一致性
+    'object-curly-newline': ['error', {
+      multiline: true,
+      consistent: true,
+    }],
+    // 以下为import模块规则
+    // 导入文件是否使用扩展名
+    // 'import/extensions': ['error', 'never'],
+    // 确保导入的文件/模块可以解析为本地文件系统上的模块(配合settings使用)
+    'import/no-unresolved': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -4,13 +4,9 @@
  * github: https://github.com/cz848/dateio#get
  */
 
-// 匹配出ISO格式的日期字符串
-const validDateRegExp = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z+$/i;
-
 // 转换为可识别的日期格式
 const toDate = input => {
   if (!(input || input === 0)) return new Date();
-  // fix: ISOString
   if (typeof input === 'string' && !/Z$/i.test(input)) return new Date(input.replace(/-/g, '/'));
   // TODO: 与原生行为有出入
   if (Array.isArray(input) && input.length !== 1) return new Date(...input);
@@ -19,10 +15,6 @@ const toDate = input => {
 
 class DateIO {
   constructor(input) {
-    this.init(input);
-  }
-
-  init(input) {
     this.$date = toDate(input);
   }
 
@@ -30,16 +22,20 @@ class DateIO {
   get(unit) {
     if (!unit) return;
     const d = this.$date;
-    // 转换成中国时区并输出'2019-10-10T15:10:22:123Z'的形式，再解析出所需要的数值
     let res;
-    if (/^[wu]$/i.test(unit)) res = {
-      w: d.getDay(),
-      W: '日一二三四五六'[d.getDay()],
-      u: +d,
-      U: Math.round(d / 1000),
-    }[unit];
-    else res = new Date(+d - d.getTimezoneOffset() * 6e4).toISOString()
-      .replace(validDateRegExp, (...arg) => arg['_YMDHISMS'.search(unit.toUpperCase())] || '');
+    if (/^[wu]$/i.test(unit)) {
+      res = {
+        w: d.getDay(),
+        W: '日一二三四五六'[d.getDay()],
+        u: +d,
+        U: Math.round(d / 1000),
+      }[unit];
+    } else {
+      // 转换成中国时区并输出'2019-10-10T15:10:22:123Z'的形式，再解析出所需要的数值
+      res = new Date(+d - d.getTimezoneOffset() * 6e4).toISOString()
+        .replace(/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z+$/i,
+          (...arg) => arg['_YMDHISMS'.search(unit.toUpperCase())] || '');
+    }
     if (/^(?:ms|[ymdhis])$/.test(unit)) res = Number(res);
     return res || undefined;
   }
